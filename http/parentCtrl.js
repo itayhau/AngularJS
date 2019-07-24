1c20acd0275582b67cd29c30c03dfa1d061387c8
itayhau <itayhau@gmail.com>
2019-07-24 09:05:38 +0300

    Add files via upload

--- http/parentCtrl.js
+++ http/parentCtrl.js
@@ -6,11 +6,16 @@ module.controller("parentCtrl", ParentCtrl)
 // DI dependency injection - IOC
 function ParentCtrl($scope, $rootScope, $http) {
 
-    let self = $scope
+    //let self = $scope
     $scope.todos = []
-    $http.get('https://jsonplaceholder.typicode.com/posts').then( function(resp) {
+    this.a = 5;
+    $scope.b = 12
+
+    $http.get('https://jsonplaceholder.typicode.com/posts').then(
+    (resp) => {
         console.log(resp.data)
-        self.todos = resp.data
+        $scope.todos = resp.data 
+
     },function(err) {
         console.losg(err)
     })
